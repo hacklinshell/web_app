471be27f7e27e9d9e2b9f6779c20ca16f9308ce5
张琳 <hacklinshell@gmail.com>
2019-03-04 11:23:49 +0800

    sanic 例程

--- www/app.py
+++ www/app.py
@@ -3,9 +3,10 @@ from sanic.response import json
 
 app = Sanic()
 
+
 @app.route("/")
 async def test(requret):
-    return json({"hello":"world"})
+    return json({"hello": "world"})
 
 if __name__ == "__main__":
     app.run("127.0.0.1", port=9000)
